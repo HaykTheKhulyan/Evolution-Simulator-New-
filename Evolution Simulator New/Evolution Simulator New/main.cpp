15345c2913c39eea0e38376c82a757ec5c8a11b5
HaykTheKhulyan <hkhulyan@gmail.com>
2018-12-01 00:33:32 -0800

    Added food generation

--- Evolution Simulator New/Evolution Simulator New/main.cpp
+++ Evolution Simulator New/Evolution Simulator New/main.cpp
@@ -1,23 +1,25 @@
 #include <Graphics.hpp>
+#include <vector>
 #include "Food.h"
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 800), "The Arena", sf::Style::Close | sf::Style::Titlebar);
+	// generates a new seed
 	srand(time(NULL));
 
+	// creates the window
+	sf::RenderWindow window(sf::VideoMode(800, 800), "The Arena", sf::Style::Close | sf::Style::Titlebar);
+
 	// controls how often food spawns (foodRate of 2.0 doubles food spawn rate, 4.0 quadruples, etc)
-	float foodRate = 1.0;
+	float foodRate = 1;
 
-	Food foodList[2500];
-
-
-	for (int i = 0; i < 2500; i++) {
-		Food newFood;
-		foodList[i] = newFood;
-	}
+	// stores all the food on the map. eaten food gets deleted.
+	std::vector<Food> foodVector;
 
 
 
+
+
+	// tracks the time since the window begins, incremented every loop
 	float deltaTime = 0.0f;
 	sf::Clock clock;
 
@@ -36,10 +38,15 @@ int main() {
 			}
 		}
 
+		if (deltaTime > .3 / foodRate) {
+			foodVector.emplace_back();
+			deltaTime = 0;
+		}
+
 		window.clear(sf::Color::White);
 
-		for (int i = 0; i < 2500; i++) {
-			foodList[i].Draw(window);
+		for (int i = 0; i < foodVector.size(); i++) {
+			foodVector.at(i).Draw(window);
 		}
 
 		window.display();
